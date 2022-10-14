9cfd70d30d2851518934df36d840d259b26fb35c
anh114 <108937335+anh114@users.noreply.github.com>
2022-10-14 09:07:22 +0700

    Delete News Reading  directory

--- News Reading /scripts/storage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//Local Storage
-function saveToStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
-}
-
-function getFromStorage(key) {
-  return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : [];
-}
